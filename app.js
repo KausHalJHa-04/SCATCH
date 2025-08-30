b015cd4ed3f28cce4237fd6207377567446c990b
kaushal kumar jha <jhakaushal361@gmail.com>
2025-08-31 04:52:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const productRouter = require("./routes/productsRouter")
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouter")  
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const db = require("./config/mongoose-connection");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.send("hey babu")
+});
+app.use("/owners", ownersRouter)
+app.use("/products", productRouter)
+app.use("/users", usersRouter)
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
