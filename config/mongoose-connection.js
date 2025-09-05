4185b03e1f5be469331a0c7dd73ce2bb08e7a002
kaushal kumar jha <jhakaushal361@gmail.com>
2025-09-06 03:43:40 +0530

    second commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -6,9 +6,13 @@ mongoose
   .connect(`${config.get("MONGODB_URL")}/scatch`)
   .then(function () {
     debug("connected");
+    // console.log("connected")
   })
   .catch(function (err) {
     debug(err);
   });
+ 
 
+  //$env:DEBUG="development:mongoose"; nodemon app.js
+  
 module.exports = mongoose.connection;
